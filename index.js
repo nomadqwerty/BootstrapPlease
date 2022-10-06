00cf0c4b2bd0a17a5bf9d2d9547f1f46e60f6fc7
nomadqwerty <davexmike72@gmail.com>
2022-10-06 22:31:10 +0100

    .

---
+++ index.js
@@ -0,0 +1,18 @@
+const container = document.querySelector(".container");
+const menuBtn = document.querySelector(".menuBtn");
+const searchBtn = document.querySelector(".searchBtn");
+const main1 = document.querySelector(".main1");
+
+//////////////////////////////////////
+const showdropDown = function (e) {
+  main1.classList.toggle("hidden");
+};
+menuBtn.addEventListener("click", showdropDown);
+searchBtn.addEventListener("click", showdropDown);
+container.addEventListener("click", function (e) {
+  if (e.target !== menuBtn && e.target !== searchBtn) {
+    if (!main1.classList.contains("hidden")) {
+      main1.classList.toggle("hidden");
+    }
+  }
+});
