33f05908c830dfc43d18f1b6fa673c58d8acc982
nomadqwerty <davexmike72@gmail.com>
2022-10-14 20:26:18 +0100

    .

--- tic_app/src/components/sections/Services/Info.js
+++ tic_app/src/components/sections/Services/Info.js
@@ -5,29 +5,27 @@ const TimeAndDate = () => {
     <div>
       <div className="name">
         <div className="infoName">
-          <p1>Impact Sunday Service</p1>
+          <p>Impact Sunday Service</p>
         </div>
       </div>
       <div className="timeDescription">
         <div className="info">
           <i className="fa fa-calendar" aria-hidden="true"></i>
-          <p1>Date: every Sunday</p1>
+          <p>Date: every Sunday</p>
         </div>
         <div className="info">
           <i className="fa fa-clock" aria-hidden="true"></i>
-          <p1>Time: 7:00AM</p1>
+          <p>Time: 7:00AM</p>
         </div>
       </div>
       <div className="Address">
         <div className="infoAddress">
-          <i class="fa fa-map-signs" aria-hidden="true"></i>
-          <p1>Address:</p1>
+          <i className="fa fa-map-signs" aria-hidden="true"></i>
+          <p>Address:</p>
         </div>
         <div className="infoAddress"></div>
         <div className="addressText">
-          <p1>
-            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-          </p1>
+          <p>#4 Ken Saro-Wiwa Stadium Road, Port Harcourt</p>
         </div>
       </div>
     </div>
