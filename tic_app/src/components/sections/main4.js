d4a0f679905d3662d08e28cac6cc97377267441f
nomadqwerty <davexmike72@gmail.com>
2022-11-12 15:23:59 +0100

    .

--- tic_app/src/components/sections/main4.js
+++ tic_app/src/components/sections/main4.js
@@ -1,4 +1,94 @@
 import React from "react";
+import {} from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+import "../../css/carousel.css";
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper";
+
+const Carousel = () => {
+  return (
+    <div>
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img
+            alt="carouselImage"
+            src="https://swiperjs.com/demos/images/nature-1.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            alt="carouselImage"
+            src="https://swiperjs.com/demos/images/nature-2.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            alt="carouselImage"
+            src="https://swiperjs.com/demos/images/nature-3.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            alt="carouselImage"
+            src="https://swiperjs.com/demos/images/nature-4.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            alt="carouselImage"
+            src="https://swiperjs.com/demos/images/nature-5.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            alt="carouselImage"
+            src="https://swiperjs.com/demos/images/nature-6.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            alt="carouselImage"
+            src="https://swiperjs.com/demos/images/nature-7.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            alt="carouselImage"
+            src="https://swiperjs.com/demos/images/nature-8.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            alt="carouselImage"
+            src="https://swiperjs.com/demos/images/nature-9.jpg"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
 
 const Main4 = () => {
   return (
@@ -6,7 +96,9 @@ const Main4 = () => {
       <div className="row">
         <div className="row">
           <div className="col-lg-12 col-sm-12 col-12">
-            <div className="main3"></div>
+            <div className="main5">
+              <Carousel />
+            </div>
           </div>
         </div>
       </div>
